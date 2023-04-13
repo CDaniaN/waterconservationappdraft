2a71350486cc476aef3c288b9fbb3f7cd92cdce7
CDaniaN <72101575+CDaniaN@users.noreply.github.com>
2023-04-12 18:57:17 -0700

    Project restructure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,75 @@
+<svelte:head>
+  <title>HOME | Water Conservation</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <div class="content">
+    <!--I think can change name "content" -->
+    <div class="text">
+      <!--I think can change name "text" -->
+      <h1>Save Water <br /> <span>Conserve Water</span></h1>
+      <!-- Change words -->
+      <p>
+        Water is an important source in our world. The water usage tracker app
+        gives a way for the users to track their water usage in order to promote
+        environmentally conscious behaviors. When it comes to how to aid in
+        helping the environment, people can feel overwhelmed and not know where
+        to start. This website can provide the beginning steps in order for the
+        user to start taking action to increase sustainability.
+      </p>
+      <!--Add more later -->
+    </div>
+  </div>
+
+  <div class="water">
+    <img src="/pepsi.png" alt="" />
+  </div>
+</section>
+
+<style>
+  .content {
+    width: 100%;
+    display: flex;
+    position: relative;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .content .text {
+    position: relative;
+    max-width: 600px;
+  }
+
+  .content .text h1 {
+    text-transform: uppercase;
+    font-weight: 800;
+    color: black;
+    line-height: 1.5em; /*should change*/
+    letter-spacing: 0.1em; /*might be able to change*/
+    margin-top: 80px;
+    font-size: 40px;
+  }
+
+  .content .text h1 span {
+    font-size: 50px;
+  }
+
+  .content .text p {
+    color: black;
+    font-size: 17px;
+    font-weight: 600;
+    margin-top: 40px;
+    letter-spacing: 0.1em;
+  }
+
+  .text {
+    position: absolute;
+    max-width: 430px;
+    width: 100%;
+    padding: 30px;
+    border-radius: 3px;
+    background: #f9f8f6;
+    opacity: 0.6;
+  }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,184 @@
+<script>
+  const members = [
+    {
+      img: "imgPath",
+      name: "Jahnvi Panchal",
+      description:
+        "I am a computer science major who enjoys front end development. My hobbies are reading, hiking, dancing, and playing with my puppy.",
+      email: "jahnvi@csu.fullerton.edu",
+    },
+    {
+      img: "imgPath",
+      name: "Jennah Kanan",
+      description: "TODO",
+      email: "TODO@mail.com",
+    },
+    {
+      img: "imgPath",
+      name: "Dania Nasereddin",
+      description:
+        "Hello! I'm Dania! I am a computer science major at CSUF. I love playing video games, walking my dog, and painting in my free time.",
+      email: "dnasereddin@csu.fullerton.edu",
+    },
+  ];
+</script>
+
+<svelte:head>
+  <title>ABOUT | Water Conservation</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  {#each members as { img, name, description, email }}
+    <div class="card">
+      <div class="content">
+        <img src={img} alt={name} />
+        <h2>{name}</h2>
+        <p>Team Member</p>
+        <p>{description}</p>
+        <p>{email}</p>
+      </div>
+      <a class="button" href={`mailto:${email}`} target="_blank">Contact</a>
+    </div>
+  {/each}
+</section>
+
+<!-- OLD CODE -->
+
+<!-- <div class = "rows">
+  <div class = "columns">
+    <div class = "note">
+      <img src = "img1.jpg" alt="Jahnvi" style = "width:100%">
+      <div class = "containers">
+        <h2>Jahnvi Panchal</h2>
+        <p class = "title">Team Member</p>
+        <p>I am a computer science major who enjoys front end development. My hobbies are reading, hiking, dancing, and playing with my puppy.</p>
+        <p>jahnvi@csu.fullerton.edu</p>
+        <p><button class = "buttons">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class = "columns">
+    <div class = "notes">
+      <img src = "img2.jpg" alt = "Jennah Kanan" style = "width: 100%">
+      <div class = "containers">
+        <h2> Jennah Kanan</h2>
+        <p class = "title">Team Member</p>
+        <p>Some text that describes me</p>
+        <p>example@example.com</p>
+        <p><button class = "buttons">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class = "columns">
+    <div class = "notes">
+      <img src = "img3.jpg" alt = "Dania Nasereddin" style = "width: 100%">
+      <div class = "containers">
+        <h2> Dania Nasereddin</h2>
+        <p class = "title">Team Member</p>
+        <p>Some text that describes me</p>
+        <p>example@example.com</p>
+        <p><button class = "buttons">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  @media screen and (max-width: 650px) {
+    .columns {
+      width: 100%;
+      display: block;
+    }
+  }
+
+  .columns {
+      float: left;
+      width: 33.3%;
+      margin-bottom: 16px;
+      padding: 0 8px;
+  }
+
+  .containers{
+      padding: 0 16px;
+  }
+
+  .containers::after {
+      clear: both;
+      display: table;
+      content: "";
+  }
+
+  .title{
+      color: grey;
+  }
+
+  .buttons{
+      width: 100%;
+      border: none;
+      outline: 0;
+      padding: 8px;
+      display: inline-block;
+      background-color: #000;
+      color: white;
+      text-align: center;
+      cursor: pointer;
+  }
+
+  .buttons:hover{
+      background-color: #555;
+  }
+</style> -->
+
+<style>
+  section {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 32px;
+  }
+
+  section .card {
+    display: flex;
+    flex-direction: column;
+    max-width: 320px;
+    width: 100%;
+    justify-content: space-between;
+    gap: 16px;
+    padding: 32px;
+    border-radius: 8px;
+    background-color: white;
+    filter: drop-shadow(0px 4px 16px rgba(0, 0, 0, 0.1));
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+
+  .content img {
+    width: 100%;
+    max-height: 128px;
+    height: 100%;
+    border: 1px solid black;
+    border-radius: 4px;
+    object-fit: cover;
+  }
+
+  .button {
+    padding: 8px;
+    background-color: black;
+    color: white;
+    border-radius: 4px;
+    text-align: center;
+    text-decoration: none;
+    transition: background-color 100ms ease-in-out;
+  }
+
+  .button:hover {
+    background-color: #333;
+  }
+</style>
