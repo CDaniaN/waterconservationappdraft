12af11bea8b23c79c259544d821adfc0b6e42265
CDaniaN <72101575+CDaniaN@users.noreply.github.com>
2023-05-16 13:39:23 -0700

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+npm install @supabase/supabase-js
+npm install @supabase/auth-helpers-sveltekit
+npm install @supabase/auth-ui-svelte @supabase/auth-ui-shared
+npm install --save @supabase/supabase-js
+npm install chart.js 
+npm run dev 
